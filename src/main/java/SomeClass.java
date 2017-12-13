6371af9f2f4bc31e9d7ecdcbd64770ec5fdcd2ea
Den Berezanskiy <dstiven@i.ua>
2017-12-13 14:24:49 +0200

    Add class with main method and run it done.

---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Denis Berezanskiy on 13.12.17.
+ */
+public class SomeClass {
+    public static void main(String[] args) {
+        String path = "ua/dp/skillsup/*";
+        System.out.println(path);
+    }
+
+}
