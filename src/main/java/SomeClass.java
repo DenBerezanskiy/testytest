ea1eb78e67518945f3c6808cf3f7be794a270b55
Den Berezanskiy <dstiven@i.ua>
2017-12-13 14:46:46 +0200

    Use some method of Strings class in main method -  done.

--- src/main/java/SomeClass.java
+++ src/main/java/SomeClass.java
@@ -1,9 +1,12 @@
+import com.google.common.base.Strings;
+
 /**
  * Created by Denis Berezanskiy on 13.12.17.
  */
 public class SomeClass {
     public static void main(String[] args) {
-        String path = "ua/dp/skillsup/*";
+        String path = "ua/dp/skillsup/";
+        path = Strings.repeat(path,2);
         System.out.println(path);
     }
 
