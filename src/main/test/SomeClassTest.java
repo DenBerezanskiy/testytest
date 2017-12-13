c062563966c4e7b7da30985a3b487a4d1a4ff0c1
Den Berezanskiy <dstiven@i.ua>
2017-12-13 14:53:26 +0200

    Create simple test class that will run code “Assert.assertTrue(true)” -  done.

---
+++ src/main/test/SomeClassTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Denis Berezanskiy on 13.12.17.
+ */
+public class SomeClassTest {
+    @Test
+    public void main() throws Exception {
+        Assert.assertTrue(true);
+
+    }
+
+}
